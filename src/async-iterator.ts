0d2b476093c2a00e20089487a7f28e68d79532b3
godu <arthur.weber@viacesi.fr>
2020-06-05 11:08:46 +0200

    AsyncIterator

--- src/async-iterator.ts
+++ src/async-iterator.ts
@@ -1,40 +1,63 @@
+import { just, nothing, Maybe, isNothing } from './functors/maybe';
 
-async function* of<A>(iterable: Iterable<A>) {
-  for (const item of iterable) {
-    yield item;
+export async function* of<A>(iterable: Iterable<A>) {
+  yield* iterable;
+}
+
+export async function toArray<A>(asyncIterable: AsyncIterable<A>): Promise<Array<A>> {
+  const values: Array<A> = [];
+  for await (const value of asyncIterable) {
+    values.push(value);
+  }
+  return values;
+}
+
+export async function toPromise<A>(asyncIterable: AsyncIterable<A>): Promise<A> {
+  let lastValue: Maybe<A> = nothing;
+  for await (const value of asyncIterable) {
+    lastValue = just(value);
+  }
+  if (isNothing(lastValue)) {
+    throw new Error('No value');
+  }
+  return lastValue.value;
+}
+
+export function map<A, B>(fun: (a: A) => B) {
+  return async function* (asyncIterable: AsyncIterable<A>) {
+    for await (const item of asyncIterable) {
+      yield fun(item);
+    }
   }
 }
 
-async function* map<A, B>(fun: (a: A) => B, asyncIterable: AsyncIterable<A>) {
-  for await (const item of asyncIterable) {
-    yield fun(item);
+export function fold<A, B>(fun: (a: A, b: B) => B, identity: B) {
+  return async function* (asyncIterable: AsyncIterable<A>) {
+    let accumulator = identity
+    for await (const item of asyncIterable) {
+      accumulator = fun(item, accumulator);
+    }
+    yield accumulator;
   }
 }
 
-async function* take<A>(count: number, asyncIterable: AsyncIterable<A>) {
-  let i = 0;
-  for await (const item of asyncIterable) {
-    if (i === count )return;
-    yield item;
-    i = i + 1;
+export function take<A>(count: number) {
+  return async function* (asyncIterable: AsyncIterable<A>) {
+    let i = 0;
+    for await (const item of asyncIterable) {
+      if (i === count) return;
+      yield item;
+      i = i + 1;
+    }
   }
 }
 
-const arr = function* () {
-  let i = 0;
-  while(true){
-    yield i;
-    i++;
+export function drop<A>(count: number) {
+  return async function* (asyncIterable: AsyncIterable<A>) {
+    let i = 0;
+    for await (const item of asyncIterable) {
+      if (i >= count) yield item;
+      i = i + 1;
+    }
   }
-};
-
-const result = map(
-  i => i * 2,
-  of(arr())
-);
-
-(async () => {
-  for await (const item of take(2, result)) {
-    console.log({item});
-  }
-})()
+}
--- src/functors/maybe.ts
+++ src/functors/maybe.ts
@@ -2,8 +2,8 @@ export interface Nothing {
     readonly _tag: 'Nothing'
 };
 export interface Just<A> {
-  readonly _tag: 'Just'
-  readonly value: A
+    readonly _tag: 'Just'
+    readonly value: A
 }
 export type Maybe<A> = Nothing | Just<A>;
 
@@ -17,4 +17,12 @@ export const of = just;
 export function map<A, B>(f: (a: A) => B, ma: Maybe<A>) {
     if (ma._tag === 'Nothing') return nothing;
     else return just(f(ma.value));
+};
+
+export function isNothing<A>(value: Maybe<A>): value is Nothing {
+    return value === nothing;
+};
+
+export function isJust<A>(value: Maybe<A>): value is Just<A> {
+    return !isNothing(nothing)
 };
