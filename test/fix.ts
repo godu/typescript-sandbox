9ee09d7e84029e644a427f5ea57e14ff6d245a5f
godu <arthur.weber@viacesi.fr>
2021-06-22 14:25:00 +0200

    Adds fixed tree sum

--- test/fix.ts
+++ test/fix.ts
@@ -2,6 +2,7 @@ import test, { Macro } from 'ava';
 import { fix } from '../src/fix';
 import { constant } from 'lodash/fp'
 import { force, Lazy } from '../src/data/functor/lazy';
+import { Tree, node, leaf } from '../src/data/functor/tree';
 
 
 const macro = <A>(): Macro<[Lazy<A>, A]> => (t, actual, expected) => {
@@ -35,4 +36,35 @@ test('repeat("foo")(5)', macro(),
 test('repeat(42)(2)', macro(),
     () => repeat(42)(2),
     [42, 42]
+)
+
+
+const tree: Tree<number> = node(
+    1,
+    node(
+        2,
+        node(3),
+        node(4,
+            node(5)
+        )
+    ),
+    node(6)
+);
+/*
+1
+├── 2
+│   ├── 3
+│   └── 4
+│       └── 5
+└── 6
+*/
+const sum = fix<(tree: Tree<number>) => number>(
+    rec =>
+        tree =>
+            tree._tag === 'Leaf'
+                ? 0
+                : tree.value + rec()(tree.left) + rec()(tree.right));
+test('sum(tree)', macro(),
+    () => sum(tree),
+    21
 )
