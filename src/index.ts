d5251c23a5de348a8a989c8c95366c225bca61b5
godu <arthur.weber@viacesi.fr>
2023-06-02 17:27:26 +0200

    WIP

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,74 @@ const start = async () => {
 		format: canvasFormat,
 	});
 
+	// Create a buffer with the vertices for a single cell.
+	const vertices = new Float32Array([
+		//   X,    Y
+		-0.8,
+		-0.8, // Triangle 1
+		0.8,
+		-0.8,
+		0.8,
+		0.8,
+
+		-0.8,
+		-0.8, // Triangle 2
+		0.8,
+		0.8,
+		-0.8,
+		0.8,
+	]);
+	const vertexBuffer = device.createBuffer({
+		label: 'Cell vertices',
+		size: vertices.byteLength,
+		// eslint-disable-next-line no-bitwise
+		usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+	});
+	device.queue.writeBuffer(vertexBuffer, 0, vertices);
+
+	const vertexBufferLayout: GPUVertexBufferLayout = {
+		arrayStride: 8,
+		attributes: [{
+			format: 'float32x2',
+			offset: 0,
+			shaderLocation: 0, // Position. Matches @location(0) in the @vertex shader.
+		}],
+	};
+
+	// Create the shader that will render the cells.
+	const cellShaderModule = device.createShaderModule({
+		label: 'Cell shader',
+		code: `
+						@vertex
+						fn vertexMain(@location(0) position: vec2f) -> @builtin(position) vec4f {
+							return vec4f(position, 0, 1);
+						}
+	
+						@fragment
+						fn fragmentMain() -> @location(0) vec4f {
+							return vec4f(1, 0, 0, 1);
+						}
+					`,
+	});
+
+	// Create a pipeline that renders the cell.
+	const cellPipeline = device.createRenderPipeline({
+		label: 'Cell pipeline',
+		layout: 'auto',
+		vertex: {
+			module: cellShaderModule,
+			entryPoint: 'vertexMain',
+			buffers: [vertexBufferLayout],
+		},
+		fragment: {
+			module: cellShaderModule,
+			entryPoint: 'fragmentMain',
+			targets: [{
+				format: canvasFormat,
+			}],
+		},
+	});
+
 	const encoder = device.createCommandEncoder();
 
 	const pass = encoder.beginRenderPass({
@@ -40,4 +108,5 @@ const start = async () => {
 };
 
 export {};
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 start();
